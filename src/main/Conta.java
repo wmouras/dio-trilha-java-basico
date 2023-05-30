dcff44a8e59788ec1342648306765811e148ab23
wmouras <50460997+wmouras@users.noreply.github.com>
2023-05-29 21:08:07 -0300

    trilha dio

--- src/main/Conta.java
+++ src/main/Conta.java
@@ -1,6 +1,42 @@
 package main;
 
 public class Conta {
+
     Integer numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
 }
